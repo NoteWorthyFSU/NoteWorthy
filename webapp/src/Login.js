6f34fe4604c1767c831bee29ea03f980133d7ef8
lexiewebel <abw16@my.fsu.edu>
2020-02-18 19:28:41 -0500

    log in page update

--- webapp/src/Login.js
+++ webapp/src/Login.js
@@ -5,22 +5,33 @@ class Login extends React.Component {
     constructor() {
         super();
         this.state = {
-            
+
         }
     }
 
-    
+
     render() {
         return (<div className = "loginBox">
+        <h1>
+          <center> Let's get started now! </center>
+        </h1>
+
+        <h1>
+          <center> Or create an account if not registered yet </center>
+        </h1>
+
             <form action = 'http://localhost:5000/login' method = 'POST'>
                     <input className = "userInput"required type="text" name="username" placeholder="Username"></input>
                     <br></br>
                   <input required className="userInput" type="password" name="password" placeholder="Password"></input>
                     <br></br>
-                  <button className="userInput" type="submit">Log In</button>
+                  <button className="userInput" type="submit">Sign In</button>
+
                 </form>
-            
-        </div>);   
+                <button className="userInput" type="submit">Forgot Password</button>
+
+
+        </div>);
     }
 }
 export default Login
