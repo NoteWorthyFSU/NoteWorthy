2ca12f9552526dfb5a05ab5542db374946e0634b
htorres-98 <Humbert.Torres001@gmail.com>
2020-02-19 11:27:30 -0500

    login page updates

--- webapp/src/Login.js
+++ webapp/src/Login.js
@@ -11,16 +11,15 @@ class Login extends React.Component {
  
 
     render() {
-        return (<div>
-        
+        return (<div>        
         <h1 className = "loginHeader">
           <center> Let's get started now! </center>
         </h1>
-        
+
         <h2 className = "loginSub">
           <center> Or create an account if not registered yet </center>
         </h2>
-          
+  
           <div className = "loginBox">
             <center>
             <h4 className = "noteWorthy"> NoteWorthy</h4>
@@ -30,12 +29,10 @@ class Login extends React.Component {
                   <input required className="userInput" type="password" name="password" placeholder="Password"></input>
                     <br></br>
                   <button className="userInput" type="submit">Sign In</button>
-
                 </form>
                 <button className="userInput" type="submit">Forgot Password</button>
             </center>
           </div>
-
         </div>);
     }
 }
