a98d1c06ab1894f810ebbf00c70155d598e96706
Orlando Kenny <kenny@cs.fsu.edu>
2020-02-29 16:59:45 -0500

    reg

--- api/backend.py
+++ api/backend.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template
 from flask_cors import CORS as cors
 from flask_pymongo import PyMongo
+import bcrypt
 from response import Response
 from pymongo import MongoClient
 
@@ -15,13 +16,50 @@ cors(app, resources={r"*": {"origins": "*"}}, headers=['Content-Type'], expose_h
 uri = "mongodb://app:noteworthy@backend-shard-00-00-7dz25.mongodb.net:27017,backend-shard-00-01-7dz25.mongodb.net:27017,backend-shard-00-02-7dz25.mongodb.net:27017/test?ssl=true&replicaSet=backend-shard-0&authSource=admin&retryWrites=true&w=majority"
 client = MongoClient(uri)
 
+app.secret_key = 'mysecret' 
+
 # Init MongoDB Connection and run sample query to test authentication
 app.config["MONGO_URI"] = uri
 mongo = PyMongo(app)
 
-@app.route('/html', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def home():
-  return render_template('filename.html')
+  if request.method == 'GET':   
+    return render_template('register.html')
+  if request.method == 'POST':
+    document = request.form.to_dict()
+    firstName = document['firstname']
+    lastName = document['lastname']
+    email = document['email']
+
+    rawPassword = document['password'].encode('utf-8')
+    
+    hashedPassword = bcrypt.hashpw(rawPassword, bcrypt.gensalt())
+    
+
+    user = mongo.db.users.find_one({'email': email})
+
+    if user is not None:
+      return "User already exists."
+    else:
+      mongo.db.users.insert_one({
+        'firstName': firstName,
+        'lastName': lastName,
+        'password': hashedPassword,
+        'email': email
+      })
+      verifiedUser = mongo.db.users.find_one({'email': email})
+
+      if verifiedUser is None:
+        return "The API reported an error"
+      else:
+        return ("Welcome" +  verifiedUser['firstName'] )
+
+      #redirect("http://localhost:5000/postRegister")
+
+@app.route('/postRegister')
+def postReg():
+  return("post register")
 
 @app.route('/')
 def index():
