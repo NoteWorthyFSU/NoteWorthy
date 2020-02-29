f44d1f505b550742d86de31f5ce43c2fef7240b4
Orlando Kenny <kenny@cs.fsu.edu>
2020-02-29 15:15:07 -0500

    dynam render

--- api/backend.py
+++ api/backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 from flask_cors import CORS as cors
 from flask_pymongo import PyMongo
 from response import Response
@@ -13,14 +13,15 @@ app = Flask(__name__)
 cors(app, resources={r"*": {"origins": "*"}}, headers=['Content-Type'], expose_headers=['Access-Control-Allow-Origin'], supports_credentials=True)
 
 uri = "mongodb://orlando:panama@backend-shard-00-00-7dz25.mongodb.net:27017,backend-shard-00-01-7dz25.mongodb.net:27017,backend-shard-00-02-7dz25.mongodb.net:27017/test?ssl=true&replicaSet=backend-shard-0&authSource=admin&retryWrites=true&w=majority"
-#mongodb://orlando@panama@backend-shard-00-00-7dz25.mongodb.net:27017, backend-shard-00-01-7dz25.mongodb.net:27017, backend-shard-00-02-7dz25.mongodb.net:27017/NoteWorthy?ssl=true&replicaSet=backend-shard-0&authSource=admin&retryWrites=true&w=majority
-#mongodb://orlando@panama@backend-shard-00-00-7dz25.mongodb.net:27017, backend-shard-00-01-7dz25.mongodb.net:27017, backend-shard-00-02-7dz25.mongodb.net:27017/NoteWorthy?ssl=true&replicaSet=backend-shard-0&authSource=admin&retryWrites=true&w=majority
-#mongodb://oak18b:panama@cluster0-shard-00-00-q86na.mongodb.net:27017,cluster0-shard-00-01-q86na.mongodb.net:27017,cluster0-shard-00-02-q86na.mongodb.net:27017/CreatorConnect?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
 client = MongoClient(uri)
 
 # Init MongoDB Connection and run sample query to test authentication
 app.config["MONGO_URI"] = uri
 mongo = PyMongo(app)
+
+@app.route('/html', methods=['GET', 'POST'])
+def home():
+  return render_template('filename.html')
 
 @app.route('/')
 def index():
