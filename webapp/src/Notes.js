74db2b5f1e23ba68610c9a6a3f21c029c00554e4
Yes98 <dtm17c@my.fsu.edu>
2020-03-28 19:08:59 -0400

    added multi notes

--- webapp/src/Notes.js
+++ webapp/src/Notes.js
@@ -8,9 +8,12 @@ class Notes extends React.Component {
         super();
         this.state = {
             cNotes: "",
-            notes: [],
+            notes: new Map(),
+            topics: [],
+            currentTopic: "",
+            tempTitle: "",
+            currentNote:1,
             titleSet: false,
-            title: "",
             prevKey: 0
         }
 
@@ -19,9 +22,11 @@ class Notes extends React.Component {
     render(){
 
         return(
-        <div>
-            <h1>{this.state.title}</h1>
-            {this.renderList()}
+        <div id="main">
+            <div id="render">
+                {this.renderList()}
+            </div>
+            <h1 id = "temptitle">{this.state.tempTitle}</h1>
             <p className="notes">{this.state.cNotes}</p>
             <form>
                 <textarea autofocus id="area" className = "noteBox" rows="50" cols="50"  
@@ -34,35 +39,80 @@ class Notes extends React.Component {
     }
     renderList()
     {
-        var temp = this.state.notes
-        var list = temp.map((temp)=> <li>{temp}</li>)
-        return(
-            <ul className="notes">{list}</ul>
-        )
+        var Notes = this.state.notes
+        var doc = document.getElementById("render")
+        if(doc)
+        { 
+            while(doc.firstChild)
+            {
+                doc.removeChild(doc.firstChild)
+            }
+        }
+        
+        var topics = this.state.topics
+        var notes = this.state.notes
+        var x
+        // goes through each main topic
+        for(x of topics)
+        {
+           var div = document.createElement("DIV") 
+           var title = document.createElement("H1")
+           title.appendChild(document.createTextNode(x))
+           div.appendChild(title)
+           var subNotes = document.createElement("UL")
+           subNotes.className = "notes"
+           //subNotes.style = " word-wrap:break-word;"
+           var y
+           for(y of notes.get(x))
+           {
+                // dont forget to add wrap around
+                var li = document.createElement("LI")
+                li.appendChild(document.createTextNode(y))
+                subNotes.appendChild(li)
+
+           }
+           div.appendChild(subNotes)
+           doc.appendChild(div)
+            
+        }
+
     }
+
     tab(e)
     {
         if(e.keyCode == 9)
         {
+            // checks if start of topic
             if(this.state.titleSet == false){
+                this.setState({tempTitle: ""});
                 this.setState({titleSet: true})
                 var box = document.getElementById("area")
+                var temp = this.state.notes
+                var temp2 = this.state.topics
+                temp.set(box.value,[])
+                temp2.push(box.value)
+                this.setState({notes: temp,currentTopic: box.value,
+                    topics: temp2})
                 box.value = ""
                 this.setState({prevKey: e.keyCode})
             }
-            if(this.state.prevKey != 9 && this.state.titleSet == true)
+            // checks if adding note
+            else if(this.state.prevKey != 9 && this.state.titleSet == true)
             {
                 
                 var box = document.getElementById("area")
                 var temp = this.state.notes
-                temp.push(box.value)
-                
+                temp.get(this.state.currentTopic).push(box.value)
                 this.setState({notes: temp})
                 box.value = ""
                 this.setState({prevKey: e.keyCode,cNotes: ""})
 
             }
+            // double tab new topic
             else{
+                this.setState({titleSet: false, currentTopic: ""})
+
+
 
             }
         }
@@ -72,11 +122,10 @@ class Notes extends React.Component {
         }
     }
 
-
     update(evt){
         if(this.state.titleSet == false)
         {
-            this.setState({title: evt.target.value});
+            this.setState({tempTitle: evt.target.value});
         }
         else{
         this.setState({cNotes: evt.target.value});
