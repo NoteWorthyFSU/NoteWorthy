2452d834ad13e952b0e0acb9ea7578b6458c4d4d
Tyler Gurley <32961830+TJGurley@users.noreply.github.com>
2020-04-03 18:39:23 -0400

    updated notes.js

--- webapp/src/Notes.js
+++ webapp/src/Notes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './notes.css';
+import Dashboard from './Dashboard.js'
 
 class Notes extends React.Component {
 
@@ -23,86 +24,8 @@ class Notes extends React.Component {
     render(){
         return(
         <div id="main">
-        <div>
-          <form action = 'http://localhost:3000/dashboard' method = 'POST'>
-          <html>
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-          <body>
-          <div className = "menu-bar">
-          <ul>
-          <li> <a href="http://localhost:3000/dashboard">Home</a> </li>
-          <li> <a href="#">About Us </a>
-          <div className="sub-menu-1">
-            <ul>
-              <li> <a href="http://localhost:3000/mission">Mission + Vision </a></li>
-              <li> <a href="http://localhost:3000/meetourteam">Meet Our Team</a></li>
-            </ul>
-          </div>
-          </li>
-          <li> <a href="#">Classes </a>
-          <div className="sub-menu-1">
-            <ul>
-              <li class ="hover-me"> <a href="#">Biology 1 </a> <i class="fa fa-angle-right"></i>
-              <div className="sub-menu-2">
-                <ul>
-                  <li> <a href="#">Note #1 </a></li>
-                  <li> <a href="#">Note #2</a></li>
-                  <li> <a href="#">Note #3</a></li>
-                </ul>
-              </div>
-              </li>
-              <li class ="hover-me"> <a href="#">Programming Languages</a><i class="fa fa-angle-right"></i>
-              <div className="sub-menu-2">
-                <ul>
-                  <li> <a href="#">Note #1 </a></li>
-                  <li> <a href="#">Note #2</a></li>
-                  <li> <a href="#">Note #3</a></li>
-                </ul>
-              </div>
-              </li>
-              <li class ="hover-me"> <a href="#">Spanish 3</a><i class="fa fa-angle-right"></i>
-              <div className="sub-menu-2">
-                <ul>
-                  <li> <a href="#">Note #1 </a></li>
-                  <li> <a href="#">Note #2</a></li>
-                  <li> <a href="#">Note #3</a></li>
-                </ul>
-              </div>
-              </li>
-              <li class ="hover-me"> <a href="#">Software Engineering</a><i class="fa fa-angle-right"></i>
-              <div className="sub-menu-2">
-                <ul>
-                  <li> <a href="#">Note #1 </a></li>
-                  <li> <a href="#">Note #2</a></li>
-                  <li> <a href="#">Note #3</a></li>
-                </ul>
-              </div>
-              </li>
-            </ul>
-          </div>
-          </li>
-          <li> <a href="#">Settings </a>
-            <div className="sub-menu-1">
-              <ul>
-                <li> <a href="http://localhost:3000/updateprofile">Update Profile</a></li>
-                <li> <a href="http://localhost:3000/changepassword">Change Password</a></li>
-              </ul>
-            </div>
-            </li>
-          <li> <a href="http://localhost:3000/contactus">Contact </a>
-          <div className="sub-menu-1">
-            <ul>
-              <li> <a href="http://localhost:3000/contactus">Get Support</a></li>
-            </ul>
-          </div>
-          </li>
-          <li> <a href="http://localhost:3000/">Log Out </a></li>
-          </ul>
-          </div>
-          </body>
-          </html>
-          </form>
-          </div>
+        <Dashboard>
+        </Dashboard>
             <div className ="cards" id="render">
                 {this.renderList()}
             </div>
@@ -110,7 +33,7 @@ class Notes extends React.Component {
             <p className="currentTopic">current Topic: {this.state.currentTopic}</p>
             <p className= "notes" id = "current">{this.state.cNotes}</p>
             <form action='http://localhost:5000/saveNotes' method="POST">
-                <button className="savebutton" type="submit">Submit</button>
+                <button className="savebutton" type="submit" onClick={this.doalert.bind()}>Save</button>
                 <textarea autofocus id="area" className = "noteBox" rows="50" cols="50"
                     onChange={evt => this.update(evt)} onKeyDown={evt => this.keyIn(evt)} />
             </form>
@@ -118,6 +41,11 @@ class Notes extends React.Component {
                 document.getElementById("area").focus()
             }}/>
         </div>);
+    }
+
+    doalert()
+    {
+      alert("Test");
     }
 
     renderList()
