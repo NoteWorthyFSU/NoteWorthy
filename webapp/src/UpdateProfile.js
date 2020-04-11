a69e588b1dfe16eb13954085278ad6005aafa41a
Orlando Kenny <kenny@cs.fsu.edu>
2020-04-11 19:08:12 -0400

    Update UpdateProfile.js

--- webapp/src/UpdateProfile.js
+++ webapp/src/UpdateProfile.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import './noteworthy.css'
 import Dashboard from './Dashboard.js'
@@ -8,9 +7,30 @@ class UpdateProfile extends React.Component {
   constructor() {
     super();
     this.state = {
+      first: "",
+      last: "",
+      email: "",
+      pass: "",
     }
   }
 
+  componentDidMount() 
+  {
+    Promise.all([
+      fetch('http://localhost:5000/changeInfo'),
+    ])
+      .then(([res1]) => Promise.all([res1.json()]))
+      .then(([data1]) => 
+        {  
+          var splitArr = ((data1['data']).toString()).split(" ")
+          this.setState({
+            first: splitArr[0],
+            last: splitArr[1],
+            email: splitArr[2],
+          })
+      });   
+
+  }
   render() {
 
       return (<div>
@@ -33,13 +53,13 @@ class UpdateProfile extends React.Component {
           <center>
           <h4 className = "noteWorthy"> <font size = "10"color="white"> NoteWorthy</font></h4>
           <form action = 'http://localhost:5000/changeInfo' method = 'POST'>
-                  <input className = "userInput"required type="text" name="subject" value = "" placeholder= ""></input>
+                  <input name="upFirst" className = "userInput" type="text" defaultValue={this.state.first} placeholder="Enter a new first name"></input>
                   <br></br>
-                  <input className = "userInput"required type="text" name="subject" value = "" placeholder= ""></input>
+                  <input name="upLast" className = "userInput" type="text" defaultValue={this.state.last} placeholder="Enter a new last name"></input>
                   <br></br>
-                  <input className = "userInput"required type="text" name="message" value = "" placeholder= "" ></input>
+                  <input name="upEmail" className = "userInput" type="text" defaultValue={this.state.email} placeholder="Enter a new email"></input>
                   <br></br>
-                  <input className = "userInput"required type="text" name="message" value = "" placeholder= ""></input>
+                  <input name="upPass" className = "userInput" type="password" placeholder= "Enter a new password"></input>
                   <br></br>
                 <button className="userInput" type="submit">Update Account</button>
               </form>
