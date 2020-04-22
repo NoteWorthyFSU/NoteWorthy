959ab366b15dacfa743a28944613d55f8856aa83
Orlando Kenny <kenny@cs.fsu.edu>
2020-04-21 21:00:39 -0400

    meeintg

--- webapp/src/Home.js
+++ webapp/src/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './noteworthy.css'
 import { Redirect } from 'react-router-dom';
 import Dashboard from './Dashboard.js'
+import Select from 'react-select';
 const classesList = []
 
 class Home extends React.Component {
@@ -9,11 +10,35 @@ class Home extends React.Component {
     constructor() {
         super();
         this.state = {
-          subjects: new Map()
+          subjects: new Map(),
+          currentSubject: "",
+          subjectRequested: "",
+          topicRequested: "",
       }
       this.categoryFnc = this.categoryFnc.bind(this)
-     
+      this.handleClasses = this.handleClasses.bind(this);
+      this.clickable = this.clickable.bind(this);
+      
      }
+
+
+   handleClasses = (newValue, actionMeta) => {
+     this.setState({currentSubject: newValue['Value']})
+     
+    if(newValue === null)
+    {
+      this.setState({subjectRequested: ""})
+    }
+    if(newValue !== null)
+    {
+        this.setState({actualClass: newValue['value'], subjectRequested: newValue['value'] });
+    }
+    
+    
+  };
+
+
+
 
      categoryFnc()
      {
@@ -23,7 +48,7 @@ class Home extends React.Component {
         //connect to backend 
         fetch('http://localhost:5000/userNotes'),
       ])
-        .then(([res1]) => Promise.all([res1.json()]))
+        .then(([res1,]) => Promise.all([res1.json()]))
         .then(([data1]) =>
           {
 
@@ -122,44 +147,131 @@ class Home extends React.Component {
         
      }
      
+     clickable () {
+      alert("hello")
+     }
    
      render() {
       //console.log(this.state.subjects)
-      const allNotes = []
+        const allNotes = []
 
-      for (let [subject,value] of this.state.subjects) {
-        let tmp = []
-        const topics = []
-        //console.log(subject)
-        for(let x of value)
+        for (let [subject,value] of this.state.subjects) 
         {
-          for(let [key, v] of x)
+          
+          if(subject === this.state.subjectRequested)
+          { 
+              
+                let tmp = []
+                const topics = []
+
+                for(let x of value)
+                {
+                  let i = 0
+                  for (let [key,v] of x)
+                  {
+
+                  
+                    topics.push(<div><button className="topic" onClick={ (e) =>
+                    {
+                        if(this.state.subjects)
+                        {
+                          alert("in")
+                          let div = document.getElementById(key)
+                          let temp = this.state.subjects
+                          temp = temp.get(subject)
+                          alert(temp)
+                          let x = document.createTextNode(temp[i].get(key))
+                          div.appendChild(x)
+                        }
+                        }}>{key}</button>
+                        <div id ={key}></div>
+                    </div>)
+                    i++
+                  } 
+                }
+        //console.log(subject)
+        //IF SUBJECT === THIS.STATE.REQUESTEDSUBJECT
+
+        /*
+        //allows for subject shown to be the subject requested
+        if(subject === this.state.subjectRequested)
+        {
+          for(let x of value)
           {
-            const noteLines = []
-            //console.log(key)
-            for(let z of v)
+            for(let [key, v] of x)
             {
-              noteLines.push(<h1 className="note">{z}</h1>)
-              //console.log(z)
-            }
-            topics.push(<div><h1 className="topic">{key}</h1>{noteLines}</div>)
-          }  
-        }
+              const noteLines = []
+              /*
+              //console.log(key)
+              var div = document.createElement("DIV")
+              var subNotes = document.createElement("UL")
+
+
+              for(let z of v)
+              {
+                // dont forget to add wrap around
+                var li = document.createElement("LI")
+                li.appendChild(document.createTextNode(z))
+                subNotes.appendChild(li)
+              }
+              div.appendChild(subNotes)
+              
+              var miniNotes = []
+              for(let z of v)
+              {
+                miniNotes.push(z)
+                //key!
+                noteLines.push({'topic': key, 'key': <h1 className="note">{miniNotes}</h1>})
+                
+                //console.log(z)
+              }
+           //  
+              topics.push(<div><button className="topic" onClick={(e) => 
+                {
+                  
+                  if(this.state.topicRequested === "")
+                  {
+                    this.setState({topicRequested: key})
+                  }
+                  else {this.setState({topicRequested: ""})}
+                    
+                  }}>{key}</button>
+                  
+                  {this.state.topicRequested !== "" && 
+                  <div><h1>{noteLines[0]['key']}</h1></div>
+                } 
+                
+                
+                </div>)
+          }  */
+        
         allNotes.push(<div><h1 className="subject">{subject}</h1>{topics}<br></br></div>)
+        }
       }
+      
       
       
         return (
           <div>
           <Dashboard>
           </Dashboard>
+
+          <Select className="wrapper"
+            isClearable
+            onChange={this.handleClasses}
+            
+            options={classesList}
+            placeholder="Pick a Subject"
+        />
+
+        {this.state.subjectRequested === "" && <div><h1 style={{color: 'black'}}>pick something</h1></div>}
         {/*alert((this.state.subjects).size)*/}
          {allNotes}
           
               <div class="noteworthy" id ="classes">
-                  <img src={require('./mac-folder-icons-51.png')} />
+                  {/* <img src={require('./mac-folder-icons-51.png')} />
                   <button class="btn" >Add class</button>
-                  {/*onclick = {alert("hahah")*/}
+                  onclick = {alert("hahah") */}
                   
               </div>
 
